f73e12c4beb123187923ad88c83b8f1d97488f28
MkHere-YT <58150295+MkHere-YT@users.noreply.github.com>
2020-07-05 22:59:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    TOKEN: 'Your bot token here', 
-    YT_API_KEY: 'Youtube API Key here', 
-    prefix: 'prefix here',
-    devs: ['your id/developers here']
+    TOKEN: 'NzI5NDM5MDM1MDMzOTExMzk3.XwI9Kw.cJVIAqPgVBEAN0huNi7aJ0AoWDY', 
+    YT_API_KEY: 'AIzaSyDOKoyv-BuXXyTlsjSk6kVndznSj1ssw8g', 
+    prefix: 'pk',
+    devs: ['279900251937177602']
 }
